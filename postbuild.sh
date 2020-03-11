38722b76e4ea9b9587c0cf3ab24425ce036f91d0
Graue Neko <graueneko@gmail.com>
2020-03-11 22:38:22 +0800

    修复 CI 中错误

--- postbuild.sh
+++ postbuild.sh
@@ -1,6 +1,4 @@
 ts=$(date +%s) 
-echo '{"version":'$ts'}'>./dist/aktools/version.json
-echo '{"version":'$ts'}'>./dist/aktools-old/version.json
 
 mkdir ./dist/aktools/static/js
 mkdir ./dist/aktools/static/css
@@ -15,14 +13,18 @@ sed -i -e 's#</noscript>#</noscript><script>window.data_version='$ts';</script>#
 
 
 [ -d "./dist/aktools-old/" ] && rm -rf ./dist/aktools-old
+
 cp -R ./aktools-old ./dist/
 
-sed -i -e 's#"static/#static/aktools-old/'$ts'/' ./dist/aktools-old/akchars.html
-sed -i -e 's#"static/#static/aktools-old/'$ts'/' ./dist/aktools-old/akevolve.html
-sed -i -e 's#"static/#static/aktools-old/'$ts'/' ./dist/aktools-old/akhr.html
-sed -i -e 's#"static/#static/aktools-old/'$ts'/' ./dist/aktools-old/akhrchars.html
-sed -i -e 's#"static/#static/aktools-old/'$ts'/' ./dist/aktools-old/aklevel.html
+sed -i -e 's#"static/#static/aktools-old/'$ts'/#g' ./dist/aktools-old/akchars.html
+sed -i -e 's#"static/#static/aktools-old/'$ts'/#g' ./dist/aktools-old/akevolve.html
+sed -i -e 's#"static/#static/aktools-old/'$ts'/#g' ./dist/aktools-old/akhr.html
+sed -i -e 's#"static/#static/aktools-old/'$ts'/#g' ./dist/aktools-old/akhrchars.html
+sed -i -e 's#"static/#static/aktools-old/'$ts'/#g' ./dist/aktools-old/aklevel.html
 
-[ -f "./dist/aktools/index.html-e"] && rm ./dist/aktools/index.html-e
+[ -f "./dist/aktools/index.html-e" ] && rm ./dist/aktools/index.html-e
 
 cp ./dist/aktools/index.html ./dist/aktools/404.html
+
+echo '{"version":'$ts'}'>./dist/aktools/version.json
+echo '{"version":'$ts'}'>./dist/aktools-old/version.json
