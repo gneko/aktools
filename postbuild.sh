7c083344b840337c166b9d3525d71fee0a4f0ec7
graueneko <graueneko@gmail.com>
2020-04-13 13:54:37 +0800

    修复自动导入脚本路径不对的问题。

--- postbuild.sh
+++ postbuild.sh
@@ -5,6 +5,18 @@ mkdir ./dist/aktools/static/css
 
 mv ./dist/aktools/*.js ./dist/aktools/static/js/
 mv ./dist/aktools/*.css ./dist/aktools/static/css/
+
+echo $ts
+
+sed -i -e 's#0-es5.worker.js#/static/aktools/'$ts'/js/0-es5.worker.js#g' ./dist/aktools/static/js/main-es5.js
+sed -i -e 's#1-es5.worker.js#/static/aktools/'$ts'/js/1-es5.worker.js#g' ./dist/aktools/static/js/main-es5.js
+[ -f "./dist/aktools/static/js/main-es5.js-e" ] && rm ./dist/aktools/static/js/main-es5.js-e
+
+sed -i -e 's#0-es2015.worker.js#/static/aktools/'$ts'/js/0-es2015.worker.js#g' ./dist/aktools/static/js/main-es2015.js
+sed -i -e 's#1-es2015.worker.js#/static/aktools/'$ts'/js/1-es2015.worker.js#g' ./dist/aktools/static/js/main-es2015.js
+[ -f "./dist/aktools/static/js/main-es2015.js-e" ] && rm ./dist/aktools/static/js/main-es2015.js-e
+
+
 sed -i -e '/src="http/! s#src="#src="/static/aktools/'$ts'/js/#g' ./dist/aktools/index.html
 sed -i -e 's#href="static/#href="/static/aktools/'$ts'/#g' ./dist/aktools/index.html
 sed -i -e 's#styles.css#/static/aktools/'$ts'/css/styles.css#g' ./dist/aktools/index.html
@@ -30,3 +42,5 @@ cp ./dist/aktools/index.html ./dist/aktools/404.html
 
 echo '{"version":'$ts'}'>./dist/aktools/version.json
 echo '{"version":'$ts'}'>./dist/aktools-old/version.json
+
+echo "Postbuild done."
