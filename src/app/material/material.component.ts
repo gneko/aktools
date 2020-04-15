be17845e2d791bae92cc549d02960842ca9da193
graueneko <graueneko@gmail.com>
2020-04-15 20:27:09 +0800

    修改刷图规划方案，重新加入

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -205,35 +205,36 @@ export class MaterialComponent implements OnInit {
     this.request.exclude = this.chooser.exclude;
     this.request.owned = owned;
     this.request.required = required;
-    this.planResult = this.fetchService.postJson('https://planner.penguin-stats.io/plan/',
-      this.request)
-      .subscribe(plan => {
-        this.cost = plan && plan.cost ? plan.cost : 0;
-        const stage = plan && plan.stages && plan.stages.length !== 0 ? [...plan.stages] : [];
-        const syns = plan && plan.syntheses && plan.syntheses.length !== 0 ? [...plan.syntheses] : [];
-        this.stagesText = [];
-        this.synsText = [];
-        for (const st of stage) {
-          const text = '[' + st.stage + '] x' + st.count + ' - ';
-          const itemsText = [];
-          for (const it in st.items) {
-            if (st.items[it]) {
-              itemsText.push(it + ' x' + st.items[it]);
-            }
-          }
-          this.stagesText.push(text + itemsText.join(', '));
-        }
-        for (const syn of syns) {
-          const text = '[' + syn.target + '] x' + syn.count + ' - ';
-          const itemsText = [];
-          for (const it in syn.materials) {
-            if (syn.materials[it]) {
-              itemsText.push(it + ' x' + syn.materials[it]);
-            }
-          }
-          this.synsText.push(text + itemsText.join(', '));
-        }
-      });
+    window.location.href="https://graueneko.gitee.io/planner-proxy/?data="+encodeURIComponent(JSON.stringify(this.request));
+    // this.planResult = this.fetchService.postJson('https://planner.penguin-stats.io/plan/',
+    //   this.request)
+    //   .subscribe(plan => {
+    //     this.cost = plan && plan.cost ? plan.cost : 0;
+    //     const stage = plan && plan.stages && plan.stages.length !== 0 ? [...plan.stages] : [];
+    //     const syns = plan && plan.syntheses && plan.syntheses.length !== 0 ? [...plan.syntheses] : [];
+    //     this.stagesText = [];
+    //     this.synsText = [];
+    //     for (const st of stage) {
+    //       const text = '[' + st.stage + '] x' + st.count + ' - ';
+    //       const itemsText = [];
+    //       for (const it in st.items) {
+    //         if (st.items[it]) {
+    //           itemsText.push(it + ' x' + st.items[it]);
+    //         }
+    //       }
+    //       this.stagesText.push(text + itemsText.join(', '));
+    //     }
+    //     for (const syn of syns) {
+    //       const text = '[' + syn.target + '] x' + syn.count + ' - ';
+    //       const itemsText = [];
+    //       for (const it in syn.materials) {
+    //         if (syn.materials[it]) {
+    //           itemsText.push(it + ' x' + syn.materials[it]);
+    //         }
+    //       }
+    //       this.synsText.push(text + itemsText.join(', '));
+    //     }
+    //   });
   }
 
   async copyResult() {
