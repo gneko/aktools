92433bbc50153a63dd315616d7c798bc20478513
bbaa_bbaa <bbaa_bbaa@qq.com>
2020-04-20 00:32:18 +0800

    预设导入导出

--- src/app/stage-chooser/stage-chooser.component.ts
+++ src/app/stage-chooser/stage-chooser.component.ts
@@ -25,9 +25,13 @@ export class StageChooserComponent implements OnInit {
     private fetchService: FetchService,
     private snackbar: MdcSnackbarService,
     private router: Router
-  ) {}
+  ) { }
   stageList: StageList = { preset: { zh_CN: [] } };
-  presets = [];
+  get presets(){
+    return Object.keys(this.stageList.preset).filter(
+      v => v != "default"
+    );
+  }
   choicePreset = "";
   willRenderStage: {
     Main: [string, boolean][][];
@@ -35,9 +39,21 @@ export class StageChooserComponent implements OnInit {
       [ActivityName: string]: [string, boolean][];
     };
   } = {
-    Main: [], // 主线
-    Activity: {} //活动
-  };
+      Main: [], // 主线
+      Activity: {} //活动
+    };
+  exportOrimporting = false;
+  get exportedData() {
+    let Data = {};
+    let DeletePreset = ["zh_CN", "en_US", "ja_JP", "ko_KR", "default"]
+    let TempData = Object.entries(this.stageList.preset).filter(v => !DeletePreset.includes(v[0]));
+    for (let [k, v] of TempData) {
+      Data[k] = v;
+    }
+    return JSON.stringify(Data);
+  }
+  importedData = '';
+  SavePresetName="";
   AllStageList: [string, boolean][] = [];
   ngOnInit() {
     this.stageList = this.fetchService.getLocalStorage("stageList", {
@@ -45,8 +61,49 @@ export class StageChooserComponent implements OnInit {
     });
     this.updateStage();
   }
+  reset() {
+    let Data = {};
+    let DeletePreset = ["zh_CN", "en_US", "ja_JP", "ko_KR", "default"];
+    let TempData = Object.entries(this.stageList.preset).filter(v => DeletePreset.includes(v[0]));
+    for (let [k, v] of TempData) {
+      Data[k] = v;
+    }
+    this.stageList.preset = Data;
+    this.saveStorage();
+  }
   get exclude() {
     return this.AllStageList.filter(v => !v[1]).map(v => v[0]);
+  }
+  getImportedData() {
+    const data = JSON.parse(this.importedData);
+    this.stageList.preset = Object.assign({}, this.stageList.preset, data);
+    this.saveStorage();
+    this.snackbar.show({
+      message: '导入成功。',
+      actionText: '好的',
+      multiline: false,
+      actionOnBottom: false
+    });
+  }
+  async copyExport() {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(this.exportedData);
+        this.snackbar.show({
+          message: '已复制到剪切板。',
+          actionText: '好的',
+          multiline: false,
+          actionOnBottom: false
+        });
+      } catch (err) {
+        this.snackbar.show({
+          message: '复制失败。',
+          actionText: '好吧',
+          multiline: false,
+          actionOnBottom: false
+        });
+      }
+    }
   }
   updateStage() {
     this.fetchService
@@ -63,10 +120,7 @@ export class StageChooserComponent implements OnInit {
       });
   }
   initData() {
-    this.presets = Object.keys(this.stageList.preset).filter(
-      v => v != "default"
-    );
-    this.willRenderStage={
+    this.willRenderStage = {
       Main: [], // 主线
       Activity: {} //活动
     };
@@ -90,7 +144,7 @@ export class StageChooserComponent implements OnInit {
         //活动关卡
         if (
           typeof this.willRenderStage.Activity[
-            this.stageList["ActivityName"][StageCode]
+          this.stageList["ActivityName"][StageCode]
           ] == "undefined"
         ) {
           this.willRenderStage.Activity[
@@ -102,7 +156,7 @@ export class StageChooserComponent implements OnInit {
         ].splice(
           this.FindinsertStageIndex(
             this.willRenderStage.Activity[
-              this.stageList["ActivityName"][StageCode]
+            this.stageList["ActivityName"][StageCode]
             ],
             Stage
           ),
@@ -112,12 +166,9 @@ export class StageChooserComponent implements OnInit {
       }
     }
   }
-  savePreset(PresetName){
-    PresetName=PresetName.value;
+  savePreset() {
+    let PresetName = this.SavePresetName;
     this.stageList.preset[PresetName] = this.stageList.preset.default;
-    if(!this.presets.includes(PresetName)) {
-      this.presets.push(PresetName);
-    }
     this.fetchService.setLocalStorage("stageList", this.stageList);
   }
   FindinsertStageIndex(dest: [string, boolean][], Stage: string) {
@@ -152,8 +203,8 @@ export class StageChooserComponent implements OnInit {
     }
     return Start;
   }
-  change(e: { target: { checked: boolean; }; },dest:[string, boolean]) {
-    dest[1] = e.target.checked; 
+  change(e: { target: { checked: boolean; }; }, dest: [string, boolean]) {
+    dest[1] = e.target.checked;
     this.saveStorage();
   }
   saveStorage() {
@@ -176,7 +227,7 @@ export class StageChooserComponent implements OnInit {
     if (this.choicePreset == "") return;
     this.stageList.preset.default = this.stageList.preset[this.choicePreset];
     //this.initData();
-    for(let Stage of this.AllStageList) {
+    for (let Stage of this.AllStageList) {
       Stage[1] = this.stageList.preset.default.includes(Stage[0]);
     }
     this.fetchService.setLocalStorage("stageList", this.stageList);
